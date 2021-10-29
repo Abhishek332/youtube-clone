02a198b56432959ca6192ad08edb8fd09f7af264
Abhishek332 <itsabhiofficial.21@gmail.com>
2021-10-29 19:37:22 +0530

    Fetch video data from JSON

--- src/components/RecommendedVideos/RecommendedVideos.js
+++ src/components/RecommendedVideos/RecommendedVideos.js
@@ -1,76 +1,27 @@
-import './RecommendedVideos.css';
-import VideoCard from './VideoCard/VideoCard';
+import "./RecommendedVideos.css";
+import VideoCard from "./VideoCard/VideoCard";
+import Data from "./data.json";
 
 const RecommendedVideos = () => {
-    return (
-        <div className="recommendedVideos">
-            <h2>Recommended Videos</h2>
-            <div className="recommendedVideos__videos">
-                <VideoCard
-                    title="Manike Mage Hite | Best song today"
-                    views="2.3M Views"
-                    timestamp="2 hour ago"
-                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT1nMuQamMyk1tk5z_UMKrfQVV8do9SGpeXOA=s68-c-k-c0x00ffffff-no-rj"
-                    channel="Audio Creator"
-                    image="https://i.ytimg.com/vi/3RZ7uTh_0yA/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCG-Y_XGAJEnsyVmsJzGI1_UVTNsA"
-                />
-                <VideoCard
-                    title="Ladai ladai maf kro | RJ Naved | Best Comedy"
-                    views="1.3M Views"
-                    timestamp="5 days ago"
-                    channelImage="https://yt3.ggpht.com/ytc/AKedOLQRXetxubs-EBQsjCVOTPYh_zzy0KTojgj5YmGqZg=s68-c-k-c0x00ffffff-no-rj"
-                    channel="Mirchi Murga"
-                    image="https://i.ytimg.com/an_webp/FfV-k8ZOH6I/mqdefault_6s.webp?du=3000&sqp=CJrYkIsG&rs=AOn4CLCIOflYBjDh2_tcXGUbbCOVnP78qw"
-                />
-                <VideoCard
-                    title="How to take revenge HG Among ... | Prabhuvani | sadvachan"
-                    views="44K Views"
-                    timestamp="1 week ago"
-                    channelImage="https://yt3.ggpht.com/ytc/AKedOLQAeNl9PFgUko6B9zT-EmnNj8lFOvHl-TWY9x0YQw=s68-c-k-c0x00ffffff-no-rj"
-                    channel="AMy Asharya"
-                    image="https://i.ytimg.com/an_webp/tqzaQ-mfMuo/mqdefault_6s_480x270.webp?du=3000&sqp=COb6kIsG&rs=AOn4CLCxov9xSllNZ2tf3Pu7leXu1CkPIw"
-                />
-                <VideoCard
-                    title="How to convert image into pencil art"
-                    views="1.3M Views"
-                    timestamp="1 hour ago"
-                    channelImage="https://yt3.ggpht.com/ytc/AKedOLQuhuxgbW7zW6du-WGiYASPZuyFhozXLaV7OyME=s68-c-k-c0x00ffffff-no-rj"
-                    channel="Editing CLub"
-                    image="https://i.ytimg.com/an_webp/MticYzYkofw/mqdefault_6s.webp?du=3000&sqp=CJzfkIsG&rs=AOn4CLDUCisc6q_XFczxcUh6Xd776Wdqlw"
-                />
-                <VideoCard
-                    title="Arjit Singh Song | Lambiya si judaiya"
-                    views="1.1M Views"
-                    timestamp="5 hour ago"
-                    channelImage="https://yt3.ggpht.com/ytc/AKedOLRDP1pDVWDoOU-AIR1vZdgWuEZCwH_4VleCz__65g8=s68-c-k-c0x00ffffff-no-rj"
-                    channel="T-series"
-                    image="https://i.ytimg.com/vi/EruLlK1Xe9c/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB_GWsp2vWKt50jzUbcUx2_wJICoA"
-                />
-                <VideoCard
-                    title="Best of Atif Asalam 2020"
-                    views="5.3M Views"
-                    timestamp="2 week ago"
-                    channelImage="https://yt3.ggpht.com/80fJMCyFKSvWestVcCxCawTie13pz2qJyO2fbcERuca4HQpWIaKsazWQCA1O0hGNLvWKNEvinA=s68-c-k-c0x00ffffff-no-rj"
-                    channel="Bollywood Song"
-                    image="https://i.ytimg.com/vi/e0tGinMHWX4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLClNHsNFuMS2yP0VE2RtN28PjL0Fw"
-                />
-                <VideoCard
-                    title="How many AI Agents an Unity handle"
-                    views="2.1K Views"
-                    timestamp="15 minutes ago"
-                    channelImage="https://yt3.ggpht.com/ytc/AKedOLTXn28m84AOxmQxTeenC060VPK880k2Yynaql-urw=s68-c-k-c0x00ffffff-no-rj"
-                    channel="Backeys"
-                    image="https://i.ytimg.com/an_webp/G9Otw12OUvE/mqdefault_6s_480x270.webp?du=3000&sqp=CI3IkIsG&rs=AOn4CLDSEl5VRGHMxTK9XrgU-d_jdzzkbg"
-                />
-                <VideoCard
-                    title="Students ki sbse bdi galti"
-                    views="3K Views"
-                    timestamp="1 day ago"
-                    channelImage="https://yt3.ggpht.com/PyuXRm-PjdQl6qfPfMZH54RV02a0w2cjc7CWm3fkI2sjRuopG2MeJkCgN7o1NnIjz5Yzft1brQ=s68-c-k-c0x00ffffff-no-rj"
-                    channel="Knowledge GATE"
-                    image="https://i.ytimg.com/an_webp/GJuLeGpOU5Q/mqdefault_6s.webp?du=3000&sqp=COTbkIsG&rs=AOn4CLAguB9IBISxWKG_qNefZbyHvuYspA"
-                />
-                <VideoCard
+  return (
+    <div className="recommendedVideos">
+      <h2>Recommended Videos</h2>
+      <div className="recommendedVideos__videos">
+        {Data.data.map((e, index) => {
+         return  <VideoCard
+            key={index}
+            title={e.title}
+            views={e.views}
+            timestamp={e.timestamp}
+            channelImage={e.channelImage}
+            channel={e.channel}
+            image={e.image}
+          />;
+        })}
+                    
+                    {/*
+                    
+                    
                     title="Manike Mage Hite | Best song today"
                     views="2.3M Views"
                     timestamp="2 hour ago"
@@ -101,10 +52,10 @@ const RecommendedVideos = () => {
                     channelImage="https://yt3.ggpht.com/ytc/AKedOLQAeNl9PFgUko6B9zT-EmnNj8lFOvHl-TWY9x0YQw=s68-c-k-c0x00ffffff-no-rj"
                     channel="AMy Asharya"
                     image="https://i.ytimg.com/an_webp/tqzaQ-mfMuo/mqdefault_6s_480x270.webp?du=3000&sqp=COb6kIsG&rs=AOn4CLCxov9xSllNZ2tf3Pu7leXu1CkPIw"
-                />
-            </div>
-        </div>
-    )
-}
+                /> */}
+      </div>
+    </div>
+  );
+};
 
 export default RecommendedVideos;
