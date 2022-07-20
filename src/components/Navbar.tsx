d9695265213ec475dada2e32aba397694115da3e
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-07-20 17:05:14 +0530

    Added Material UI icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className="h-16 w-full bg-yellow-500">
+        <div className="h-14 px-5 w-full bg-yellow-500 flex items-center justify-between sticky t-0 l-0">
             Porwal
         </div>
     )
