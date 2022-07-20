b1e9ba7716f65a985fe31c6d4116c8a630c7823e
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-07-20 15:34:00 +0530

    Setup tailwindCss and made a basic folder structure

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="h-16 w-full bg-yellow-500">
+            Porwal
+        </div>
+    )
+}
+
+export default Navbar
